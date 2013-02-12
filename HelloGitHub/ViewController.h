b3a0d09348350e1150c5dc11272eb465bc7611a2
hisatomiyasushi <hisatomiy@gmail.com>
2013-02-12 11:42:14 -0500

    Initial Commit

---
+++ HelloGitHub/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloGitHub
+//
+//  Created by Hisatomi Yasushi on 13/02/12.
+//  Copyright (c) 2013年 hisatomiyasushi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HelloGitHub/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloGitHub
+//
+//  Created by Hisatomi Yasushi on 13/02/12.
+//  Copyright (c) 2013年 hisatomiyasushi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
